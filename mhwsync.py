aa81bcdfadf5920badbb5183cd3d01a19215860d
Michael Walter <6783810+Kaedras@users.noreply.github.com>
2020-08-26 15:46:37 +0200

    refactoring

--- mhwsync.py
+++ mhwsync.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, json, jsonify
-import logging
-import os
 import sys
+
+from flask import Flask, request
 
 if sys.version_info[0] < 3 or sys.version_info[1] < 7:
     raise Exception("Python 3.7 or higher required")
@@ -23,35 +22,36 @@ status codes:
 ok = {
     "status": 0,
     "value": ""
-    }
-sessionDoesNotExist = { 
+}
+sessionDoesNotExist = {
     "status": 1,
     "value": "session does not exist"
-    }
+}
 sessionAlreadyExists = {
     "status": 2,
     "value": "session already exists"
-    }
-monsterOutsideRange = { 
+}
+monsterOutsideRange = {
     "status": 3,
     "value": "monster outside range"
-    }
+}
 partOutsideRange = {
-   "status": 4,
-   "value": "part outside range"
-   }
+    "status": 4,
+    "value": "part outside range"
+}
 ailmentOutsideRange = {
     "status": 5,
     "value": "ailment outside range"
-    }
+}
 e404 = {
     "status": 404,
     "value": ""
-    }
+}
 
 bufferSize = 50
 
-class monster:
+
+class Monster:
     def __init__(self):
         self.__parts = dict()
         self.__ailments = dict()
@@ -64,162 +64,160 @@ class monster:
             self.__parts[i] = 0
             self.__ailments[i] = 0
 
-    def getParts(self):
-        return self.__parts
-
-    def getAilments(self):
-        return self.__ailments
-
-    def getPart(self, index):
+    def get_part(self, index):
         if len(self.__parts) < index + 1:
             return None
         return self.__parts[index]
-        
-    def getAilment(self, index):
+
+    def get_ailment(self, index):
         if len(self.__ailments) < index + 1:
             return None
         return self.__ailments[index]
 
-    def getPartCount(self):
-        return len(self.__parts)
-        
-    def getAilmentCount(self):
-        return len(self.__ailments)
+    def set_part_hp(self, index, hp):
+        self.__parts[index] = hp
 
-    def setPartHP(self, id, hp):
-        self.__parts[id] = hp
-
-    def setAilmentBuildup(self, id, hp):
-        self.__ailments[id] = hp
+    def set_ailment_buildup(self, index, hp):
+        self.__ailments[index] = hp
 
 
-class session:
+class Session:
     def __init__(self):
-        self.__monsters = [monster(), monster(), monster()]
+        self.__monsters = [Monster(), Monster(), Monster()]
 
-    def clearMonster(self, index):
+    def clear_monster(self, index):
         self.__monsters[index].clear()
 
-    def getMonsters(self):
-        return self.__monsters
-
-    def getMonster(self, index):
+    def get_monster(self, index):
         return self.__monsters[index]
+
 
 sessions = []
 session_dict = dict()
+
 
 @app.route('/')
 def alive():
     ok["value"] = ""
     return ok
 
-@app.route('/session/<string:id>/exists')
-def sessionInfo(id):
-    if id in session_dict:
+
+@app.route('/session/<string:session>/exists')
+def session_info(session):
+    if session in session_dict:
         ok["value"] = ""
         return ok
     return sessionDoesNotExist
 
-@app.route('/session/<string:id>/create')
-def createSession(id):
-    if id in session_dict:
+
+@app.route('/session/<string:session>/create')
+def create_session(session):
+    if session in session_dict:
         return sessionAlreadyExists
     index = len(sessions)
-    sessions.append(session())
-    sessions[index].id = id
-    session_dict[id] = index
+    sessions.append(Session())
+    sessions[index].id = session
+    session_dict[session] = index
     ok["value"] = ""
     return ok
 
-@app.route('/session/<string:id>/delete')
-def deleteSession(id):
-    if id not in session_dict:
+
+@app.route('/session/<string:session>/delete')
+def delete_session(session):
+    if session not in session_dict:
         return sessionDoesNotExist
-    sessions.remove(sessions[session_dict[id]])
-    session_dict.pop(id)
+    sessions.remove(sessions[session_dict[session]])
+    session_dict.pop(session)
     ok["value"] = ""
     return ok
 
-@app.route('/session/<string:id>/monster/<int:index>/clear')
-def clearMonster(id, index):
-    if id not in session_dict:
-        return sessionDoesNotExist
-    if index not in range(3):
-        return monsterOutsideRange
-    session = sessions[session_dict[id]]
-    session.clearMonster(index)
-    ok["value"] = ""
-    return ok
 
-@app.route('/session/<string:id>/monster/<int:index>/part/<int:partindex>/hp')
-def getPartHP(id, index, partindex):
-    if id not in session_dict:
+@app.route('/session/<string:session>/monster/<int:index>/clear')
+def clear_monster(session, index):
+    if session not in session_dict:
         return sessionDoesNotExist
     if index not in range(3):
         return monsterOutsideRange
-    if partindex not in range(bufferSize):
+    session = sessions[session_dict[session]]
+    session.clear_monster(index)
+    ok["value"] = ""
+    return ok
+
+
+@app.route('/session/<string:session>/monster/<int:index>/part/<int:part_index>/hp')
+def get_part_hp(session, index, part_index):
+    if session not in session_dict:
+        return sessionDoesNotExist
+    if index not in range(3):
+        return monsterOutsideRange
+    if part_index not in range(bufferSize):
         return partOutsideRange
-    mon = sessions[session_dict[id]].getMonster(index)
-    part = mon.getPart(partindex)
+    mon = sessions[session_dict[session]].get_monster(index)
+    part = mon.get_part(part_index)
     if part is None:
         return partOutsideRange
     ok["value"] = str(part)
     return ok
 
-@app.route('/session/<string:id>/monster/<int:index>/part/<int:partindex>/hp/<int:value>')
-def setPartHP(id, index, partindex, value):
-    if id not in session_dict:
+
+@app.route('/session/<string:session>/monster/<int:index>/part/<int:part_index>/hp/<int:value>')
+def set_part_hp(session, index, part_index, value):
+    if session not in session_dict:
         return sessionDoesNotExist
     if index not in range(3):
         return monsterOutsideRange
-    if partindex not in range(bufferSize):
+    if part_index not in range(bufferSize):
         return partOutsideRange
-    mon = sessions[session_dict[id]].getMonster(index)
-    mon.setPartHP(partindex, value)
+    mon = sessions[session_dict[session]].get_monster(index)
+    mon.set_part_hp(part_index, value)
     ok["value"] = ""
     return ok
 
-@app.route('/session/<string:id>/monster/<int:index>/ailment/<int:ailmentindex>/buildup')
-def getAilmentBuildup(id, index, ailmentindex):
-    if id not in session_dict:
+
+@app.route('/session/<string:session>/monster/<int:index>/ailment/<int:ailment_index>/buildup')
+def get_ailment_buildup(session, index, ailment_index):
+    if session not in session_dict:
         return sessionDoesNotExist
     if index not in range(3):
         return monsterOutsideRange
-    if ailmentindex not in range(bufferSize):
+    if ailment_index not in range(bufferSize):
         return ailmentOutsideRange
-    mon = sessions[session_dict[id]].getMonster(index)
-    ailment = mon.getAilment(ailmentindex)
+    mon = sessions[session_dict[session]].get_monster(index)
+    ailment = mon.get_ailment(ailment_index)
     if ailment is None:
         return ailmentOutsideRange
     ok["value"] = str(ailment)
     return ok
 
-@app.route('/session/<string:id>/monster/<int:index>/ailment/<int:ailmentindex>/buildup/<int:value>')
-def setAilmentBuildup(id, index, ailmentindex, value):
-    if id not in session_dict:
+
+@app.route('/session/<string:session>/monster/<int:index>/ailment/<int:ailment_index>/buildup/<int:value>')
+def set_ailment_buildup(session, index, ailment_index, value):
+    if session not in session_dict:
         return sessionDoesNotExist
     if index not in range(3):
         return monsterOutsideRange
-    if ailmentindex not in range(bufferSize):
+    if ailment_index not in range(bufferSize):
         return ailmentOutsideRange
-    mon = sessions[session_dict[id]].getMonster(index)
-    mon.setAilmentBuildup(ailmentindex, value)
+    mon = sessions[session_dict[session]].get_monster(index)
+    mon.set_ailment_buildup(ailment_index, value)
     ok["value"] = ""
     return ok
 
+
 @app.errorhandler(404)
-def not_found(error=None):
+def not_found():
     e404["value"] = request.url
     return e404
+
 
 @app.errorhandler(Exception)
 def handle_exception(e, source="app-errorhandler"):
     msg = {
         "status": 10,
         "value": repr(e) + ", source: " + source + ", request: " + request.full_path
-        }
+    }
     return msg
+
 
 if __name__ == "__main__":
     app.run()
